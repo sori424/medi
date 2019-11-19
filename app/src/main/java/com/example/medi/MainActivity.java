57408532090a91987e122710bc3cfb3aa258f240
sori424 <47997074+sori424@users.noreply.github.com>
2019-11-19 17:19:27 +0900

    camera test 추가

--- app/src/main/java/com/example/medi/MainActivity.java
+++ app/src/main/java/com/example/medi/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.medi;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 import static android.content.ContentValues.TAG;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PhotoFragment.OnFragmentInteractionListener {
 
     SQLiteDatabase db;
+    int PERMISSION_ALL = 1;
+    boolean flagPermissions = false;
+
+    String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +41,48 @@ public class MainActivity extends AppCompatActivity {
 
         db.close();
         databaseHelper.close();
+
+        ButterKnife.bind(this);
+        checkPermissions();
+    }
+
+    @OnClick(R.id.make_photo_button)
+    void onClickScanButton(){
+        if (!flagPermissions){
+            checkPermissions();
+            return;
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.res_photo_layout, new PhotoFragment()).addToBackStack(null).commit();
+    }
+
+    @SuppressLint("NewApi")
+    void checkPermissions(){
+        if (!hasPermissions(this, PERMISSIONS)){
+            requestPermissions(PERMISSIONS, PERMISSION_ALL);
+            flagPermissions = false;
+        }
+        flagPermissions = true;
+    }
+
+    public static boolean hasPermissions(Context context, String... permissions){
+        if (context != null && permissions != null){
+            for(String permission : permissions){
+                if(ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onFragmentInteraction(Bitmap bitmap) {
+        if(bitmap != null){
+            ImageFragment imageFragment = new ImageFragment();
+            imageFragment.imageSetupFragment(bitmap);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.res_photo_layout, imageFragment).addToBackStack(null).commit();
+        }
     }
 
     void DBInsert(String tableName, String medName, Integer medID, String medFunc, String medHow, String medCau) {
