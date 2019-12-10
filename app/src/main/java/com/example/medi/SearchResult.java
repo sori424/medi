c4d8415acdfe0074ab5ba1350485511dfbf9b0a9
hybae430 <violet9643@naver.com>
2019-12-10 17:11:11 +0900

    1210 낮(수정)

---
+++ app/src/main/java/com/example/medi/SearchResult.java
@@ -0,0 +1,15 @@
+package com.example.medi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchResult extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchresult);
+    }
+
+
+}
